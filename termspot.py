0520b8c60479399af671a105cf6ce84a1b1cfa9a
Kazulhu <102474945+Kazulhu@users.noreply.github.com>
2023-05-29 15:47:03 +0200

    Update termspot.py

--- termspot.py
+++ termspot.py
@@ -3,108 +3,111 @@
 #    2022 - 2023     #
 ######################
 
-import os
 import discord
-import asyncio
-import language_tool_python
 from discord.ext import commands
-from discord.ui import Button, View
-import datetime
+import language_tool_python
+from random import randint
+import asyncio
+import os
 
-# Initialiser le client discord
-intents = discord.Intents.all()
-intents.message_content = True
-client = discord.Client(intents=intents)
 tool = language_tool_python.LanguageTool('fr')
-bot = commands.Bot(command_prefix='!',intents=intents)
-validity_duration = 20
-command_prefix = '!corrige '
-command_stats = '!stats'
-command_lang = '!lang '
-total_fautes_corrige = 0
+global total_fautes_corrige  # Number of mistakes corrected
+intents = discord.Intents.default()
+intents.message_content = True
 
-class EphemeralButton(discord.ui.Button):
-    def __init__(self, label, *, style=discord.ButtonStyle.grey):
-        super().__init__(style=style, label=label)
+# Generation of a forbidden characters list
+forbidden_list = []
+for i in range(8):
+    forbidden_list.append(chr(randint(97, 122)))
+forbidden_word = ''.join(forbidden_list)
+print(f"Here is the forbidden word : '{forbidden_word}'")
 
-    async def callback(self, interaction: discord.Interaction):
-        global mistakes, correct, msg
-        if correct==mistakes==0:
-            await interaction.response.send_message("Pas d'erreurs, toutes mes félicitations",ephemeral=True)
-            await interaction.channel.delete_messages([interaction.message])
-        else:
-            await interaction.response.send_message(f"{mistakes}Voici ton message automatiquement corrigé:\n{correct}", ephemeral=True)
-            await msg.delete()
-            await interaction.channel.delete_messages([interaction.message])
+client = discord.Bot(intents=intents)
 
+# Path to the file for storing error count
+ERROR_COUNT_FILE = 'error_count.txt'
 
-# Lorsque le bot se lance
+def load_error_count():
+    if os.path.isfile(ERROR_COUNT_FILE):
+        with open(ERROR_COUNT_FILE, 'r') as file:
+            try:
+                return int(file.read())
+            except ValueError:
+                return 0
+    else:
+        return 0
+
+def save_error_count(count):
+    with open(ERROR_COUNT_FILE, 'w') as file:
+        file.write(str(count))
+
+# Load the initial error count
+total_fautes_corrige = load_error_count()
+
 @client.event
 async def on_ready():
     print('Connecté en tant que {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name="corriger vos fautes"))
-    #general_channel = client.get_channel(1083734674611912754)      #idea: 1083734674611912754
-                                                                    #termspot: 1083361373678997675
-    #await general_channel.send("Bonjour tout le monde, je suis en ligne !\nVous pouvez me ping avec <@1083359393376120892> et m'envoyer un message et je vous dirai s'il y a une erreur !")
 
+def text_correction(text):
+    global total_fautes_corrige
+    matches = tool.check(text)
+    total_fautes_corrige += len(matches)
+    save_error_count(total_fautes_corrige)
+    if matches:
+        corrected_text = tool.correct(text)
+        corrected_list = corrected_text.split(' ')
+        for i in range(len(corrected_list)):
+            corrected_text = tool.correct(corrected_text)
+        return corrected_text
 
-# A chaque message envoyé
-@client.event
-async def on_message(message):
-    global total_fautes_corrige, mistakes, correct, msg, tool
-
-    # Si l'auteur du message est le bot lui-même ne rien faire
-    if message.author == client.user:
-        return
-
-    # Si le message a bien été envoyé dans un chat textuel
-    if not isinstance(message.channel, discord.TextChannel):
-        return
-
-    # Vérifier si le message commence par le préfixe de la commande personnalisée (stats)
-    if message.content.startswith(command_stats):
-        await message.reply(f'Depuis que je suis réveillé il y a eu {total_fautes_corrige} fautes.')
-
-    elif message.content.startswith(command_lang):
-        text = message.content[len(command_lang):]
-        if text in tool.language.languages:
-            tool = language_tool_python.LanguageTool(text)
-            await message.reply(f"The language has been changed to {text}")
-        else:
-            await message.reply("I don't know this language.")
-
-    # Vérifier si le message commence par le préfixe corrige
-    elif message.content.startswith(command_prefix):
-        text = message.content[len(command_prefix):]
-        matches = tool.check(text)
-        total_fautes_corrige += len(matches)  # Ajouter le nombre de fautes pour les stats
-        if matches:
-            corrected_text = tool.correct(text)
-            # Afficher les règles oubliées par l'utilisateur
-            response = f"{message.author.mention} Voici les règles oubliées dans ton message originel : ''{matches[0].context}''\n"
-            for correction in matches:
-                response += f"- {correction.message}\n"
-            response += "\n"
-            mistakes = response
-            new_correct = tool.correct(corrected_text)
-            correct = tool.correct(new_correct)
-        else:
-            mistakes=0
-            correct=0
-        msg=message
-        view = discord.ui.View()
-        view.add_item(EphemeralButton("Clique ici tu as 20 secondes pas encore mais bientôt t'auras que 20 secondes!"))
-        await message.reply("Clique si tu veux savoir s'il y a des erreurs ! S'il y en a, ton message originel sera supprimé.", view=view)
-
+def mistakes(text):
+    global total_fautes_corrige
+    matches = tool.check(text)
+    total_fautes_corrige += len(matches)
+    save_error_count(total_fautes_corrige)
+    if matches:
+        # Afficher les règles oubliées par l'utilisateur
+        response = f"Voici les règles oubliées dans ton message originel : ''{matches[0].context}''\n"
+        for correction in matches:
+            response += f"- {correction.message}\n"
+        response += "\n"
+        mistakes = response
     else:
-        # Si aucune commandes alors vérifier le texte et le corriger
-        matches = tool.check(message.content)
-        total_fautes_corrige += len(matches)
-        if matches:
-            new_text = tool.correct(message.content)
-            new_correct_text = tool.correct(new_text)
-            corrected_text = tool.correct(new_correct_text)
-            await message.delete()
-            await message.channel.send(f"{message.author.display_name.split('#')[0]}: {corrected_text}")
+        mistakes = 0
+    return mistakes
 
-    await asyncio.sleep(1) # Ajouter un délai de 1 seconde entre chaque message traité
+@client.slash_command(name='cor', description='Send your message with no mistakes')
+async def cor_command(ctx: commands.Context, text: str):
+    await ctx.respond("Le message corrigé devrait être envoyé d'ici quelques secondes.",ephemeral=True)
+    await ctx.channel.send(f"***{ctx.author.display_name.split('#')[0]} :*** {text_correction(text)}")
+
+
+@client.slash_command(name='exp', description='Corrects the given text and provides error suggestions')
+async def exp_command(ctx: commands.Context, text: str):
+    answer = mistakes(text)
+    if answer == 0:
+        await ctx.respond(content="Pas d'erreurs, toutes mes félicitations",ephemeral=True)
+    else:
+        await ctx.respond(content=answer,ephemeral=True)
+
+@client.slash_command(name='stats', description='Shows the number of errors corrected by the bot')
+async def stats_command(ctx: commands.Context):
+    await ctx.respond(f'Depuis que je suis publié il y a eu {total_fautes_corrige} fautes.')
+
+@client.slash_command(name='help', description="Shows all the commands of TermSpot(it's me)")
+async def help_command(ctx: commands.Context):
+    embed = discord.Embed(
+        title="Commandes",
+        description="Voici toutes les commandes que tu peux utiliser grace à moi:",
+        color=discord.Colour.blurple(),  # Pycord provides a class with default colors you can choose from
+    )
+    embed.add_field(name="/cor <insert text>", value="J'envoie ton message à ta place mais corrigé", inline=True)
+    embed.add_field(name="/exp <insert text>", value="Envoie les erreurs commises dans ton message", inline=True)
+    embed.add_field(name="/stats", value="Envoie le nombre d'erreurs corrigées par le bot depuis qu'il est en ligne", inline=True)
+    embed.add_field(name="/help", value="Tu viens d'utiliser cette commande", inline=True)
+    embed.set_author(name="TermSpot", icon_url="https://i.imgur.com/obQVXHO.jpg")
+    embed.set_thumbnail(url="https://i.imgur.com/obQVXHO.jpg")
+    await ctx.respond(embed=embed)
+
+client.run('token')
